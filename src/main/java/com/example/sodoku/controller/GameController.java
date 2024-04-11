e9f54ef1f3316212f05befb11794bff798ac0d9c
james bliner m quijano <104052352+jamesbliner1@users.noreply.github.com>
2024-04-11 17:17:39 -0500

    MATRIZ INICIALIZADA 1

--- src/main/java/com/example/sodoku/controller/GameController.java
+++ src/main/java/com/example/sodoku/controller/GameController.java
@@ -27,29 +27,51 @@ public class GameController {
 
     public void setWord(String namePlayer) {
         this.namePlayer = namePlayer.toUpperCase();
-        idtitulo.setText(this.namePlayer+" a Sodokjugar");
+        idtitulo.setText(this.namePlayer + " a Sodokjugar");
     }
 
     // traigo de sopa de letras
 
     @FXML
-    public void initialize(){
+    public void initialize() {
+        // inicializo la matriz en ceros
+       inicializeArrayZeros();
         //cuadro 1
 
-        int numerosIniciales =4;
-        int contadorNumerosIniciales=0;
+        int numerosIniciales = 4;
+        int contadorNumerosIniciales = 0;
+        int x=0, y=0, cuadroX=0, cuadroY=0, valueXY=0;
+        for (int i = 0; i < 4; i++) {
 
-        for (int i=0; i<3; i++){
-            for (int j=0; j<3; j++){
+                x=generarNumeroAleatorio(2);
+                y=generarNumeroAleatorio(2);
+                valueXY=this.array[x][y];
+                if(valueXY== 0 ) {
+                    verificarXY(x,y,cuadroX,cuadroY);
+                }
                 TextField textFieldLetter = new TextField();
-                gridPaneTablero.add(textFieldLetter, i, j);
-                textFieldEnterText(textFieldLetter, i, j);
-                textFieldLetter.setText(""+generarNumeroAleatorio(9));
-            }
+                gridPaneTablero.add(textFieldLetter, x, y);
+                textFieldEnterText(textFieldLetter, x, y);
+                textFieldLetter.setText("" + generarNumeroAleatorio(9));
+
         }
     }
 
-    private void textFieldEnterText(TextField textFieldLetter, int i, int j){
+    private void verificarXY(int x, int y, int cuadroX, int cuadroY) {
+        int valueXY=this.array[x][y];
+        this.array[x][y]=generarNumeroAleatorio(9);
+        verificaCuadro(int x, int y, int cuadroX, int cuadroY);
+
+    }
+
+    private void verificaCuadro(int x, int y, int cuadroX, int cuadroY){
+        int contador=0;
+        for(int i= x; i<(x + 3); i++ ){
+            for(int j=y; j<(y+3); j++){
+               contador= contador+1;
+        }
+    }
+    private void textFieldEnterText(TextField textFieldLetter, int i, int j) {
         textFieldLetter.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
@@ -60,23 +82,24 @@ public class GameController {
     }
     // traigo de sopa de letras
 
-    private int generarNumeroAleatorio(int rango){
+    private int generarNumeroAleatorio(int rango) {
         // Generar un número aleatorio entre 1 y 9 (ambos inclusive)
         Random aleatorio = new Random();
-        int numeroAleatorio= aleatorio.nextInt(rango)+1;
+        int numeroAleatorio = aleatorio.nextInt(rango) + 1;
         // Imprimir el número generado
-        System.out.println("Número aleatorio entre 1 y"+rango+ ": " + numeroAleatorio);
+        System.out.println("Número aleatorio entre 1 y" + rango + ": " + numeroAleatorio);
         return numeroAleatorio;
     }
 
-    private int[][]  inicializeArrayZeros(int[][] array)
-    {
-            for (int i=0;i<9;i++){
-                for (int j=0;j<9;j++){
-                    array[i][j] = 0;
-                }
+    private void inicializeArrayZeros() {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                this.array[i][j] = 0;
             }
-        return array;
+        }
+
     }
     //
+
+
 }
